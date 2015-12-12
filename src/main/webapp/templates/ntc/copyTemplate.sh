f97d580135d6ac1d7b285f1af7d33d37436c7afd
Nick Robinson <nickthecoder@gmail.com>
2015-12-12 13:20:45 +0000

    Ready for rc1

---
+++ src/main/webapp/templates/ntc/copyTemplate.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+cp *.png *.jsp *.js /home/nick/server/projects/new/pinkwino/pinkwino-application/src/main/webapp/templates/ntc/
+
+cp *.png *.jsp *.js /home/nick/server/projects/new/gidea/src/main/webapp/templates/ntc/
+
+cp *.png *.jsp *.js /home/nick/server/projects/ichneutae/src/main/webapp/templates/ntc/
+
